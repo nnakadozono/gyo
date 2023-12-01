de15414ef883316d34e35a7171e2839bbe926437
Naoyuki Nakadozono <nnakadozono@users.noreply.github.com>
2023-12-01 22:54:28 +0900

    update terraform

--- gyo-app/terraform/project.tf
+++ gyo-app/terraform/project.tf
@@ -4,16 +4,6 @@ resource "vercel_project" "gyo-app" {
   git_repository = {
     type = "github"
     repo = "nnakadozono/gyo"
+    root_directory = "gyo-app"
   }
-}
-
-data "vercel_project_directory" "gyo-app" {
-  path = "../"
-}
-
-resource "vercel_deployment" "gyo-app" {
-  project_id  = vercel_project.gyo-app.id
-  files       = data.vercel_project_directory.gyo-app.files
-  path_prefix = "../"
-  production  = true
 }
